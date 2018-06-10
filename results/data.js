d78c53f107d254361c1e13d342c417c0f72afc0c
Nathan Hunt <neighthan.hunt@gmail.com>
2018-06-10 07:52:43 -0700

    Added example data file

---
+++ results/data.js
@@ -0,0 +1,7 @@
+let data = [
+  {nu: 0.01, lambda_r: 0.1, tau_r: 2e-6, msd: 11, vecFilepath: 'test_img1/1.png', finaltimeFilepath: 'test_img2/vec_1.png'},
+  {nu: 0, lambda_r: 1, tau_r: 2e-6, msd: 14, vecFilepath: 'test_img1/2.png', finaltimeFilepath: 'test_img2/vec_2.png'},
+  {nu: .05, lambda_r: 1.5, tau_r: 4e-6, msd: 8, vecFilepath: 'test_img1/3.png', finaltimeFilepath: 'test_img2/vec_3.png'},
+  {nu: 0, lambda_r: 0.5, tau_r: 1e-6, msd: 27, vecFilepath: 'test_img1/4.png', finaltimeFilepath: 'test_img2/vec_4.png'},
+  {nu: 0.1, lambda_r: 1, tau_r: 8e-6, msd: 17, vecFilepath: 'test_img1/5.png', finaltimeFilepath: 'test_img2/vec_5.png'} ,
+]
